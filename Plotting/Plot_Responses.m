792feb06fd63cc9577d65101684a28ad6d39e32a
Thomas Mueller <thomas-harald.mueller1@outlook.de>
2026-02-18 16:50:12 +0100

    Add files via upload

--- Plotting/Plot_Responses.m
+++ Plotting/Plot_Responses.m
@@ -1,9 +1,13 @@
 function Plot_Responses(OUT, Sim, SAVE_EPS, OUTDIR, suffix)
 
 style = PlotStyle();
+t     = OUT.time;
+munc  = Sim.mode.muncertainty;
 
-t = OUT.time;
-munc = Sim.mode.muncertainty;
+%% LaTeX defaults (2018b compatible)
+set(groot,'defaultTextInterpreter','latex')
+set(groot,'defaultAxesTickLabelInterpreter','latex')
+set(groot,'defaultLegendInterpreter','latex')
 
 %% ========================================================================
 % HEADING + FLIGHT PATH
@@ -13,42 +17,33 @@ figure('Color',style.Background,...
        'Units','centimeters',...
        'Position',[5 5 style.FigureWidthExtraWideCm style.FigureHeightCm]);
 
-tiledlayout(1,2)
-
-% --- Altitude ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(1,2,1); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.h, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.h','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.h','Color',[0.6 0.6 1])
 end
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Altitude (m)','Interpreter',style.Interpreter)
-title('Altitude Response','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Altitude (m)')
+title('Altitude Response')
 
-% --- Flight Path ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(1,2,2); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.flight, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.flight','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.flight','Color',[0.6 0.6 1])
 end
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Flight Path $\gamma$ (deg)','Interpreter',style.Interpreter)
-title('Flight Path Response','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Flight Path $\gamma$ (deg)')
+title('Flight Path Response')
 
 if SAVE_EPS
-    exportgraphics(gcf, ...
-        fullfile(OUTDIR, ['Heading_FlightPath' suffix '.pdf']), ...
-        'ContentType','vector')
+    savePDF(gcf, fullfile(OUTDIR,['Heading_FlightPath' suffix]))
 end
-
 
 %% ========================================================================
 % AIRSPEED + THROTTLE
@@ -58,42 +53,33 @@ figure('Color',style.Background,...
        'Units','centimeters',...
        'Position',[5 5 style.FigureWidthExtraWideCm style.FigureHeightCm]);
 
-tiledlayout(1,2)
-
-% --- Airspeed ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(1,2,1); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.speed, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.speed','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.speed','Color',[0.6 0.6 1])
 end
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Airspeed $V$ (m/s)','Interpreter',style.Interpreter)
-title('Airspeed Response','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Airspeed $V$ (m/s)')
+title('Airspeed Response')
 
-% --- Throttle ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(1,2,2); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.throttle, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.throttle','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.throttle','Color',[0.6 0.6 1])
 end
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Throttle (-)','Interpreter',style.Interpreter)
-title('Throttle Command','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Throttle (-)')
+title('Throttle Command')
 
 if SAVE_EPS
-    exportgraphics(gcf, ...
-        fullfile(OUTDIR, ['Airspeed_Throttle' suffix '.pdf']), ...
-        'ContentType','vector')
+    savePDF(gcf, fullfile(OUTDIR,['Airspeed_Throttle' suffix]))
 end
-
 
 %% ========================================================================
 % ANGLE OF ATTACK + SIDESLIP
@@ -103,44 +89,35 @@ figure('Color',style.Background,...
        'Units','centimeters',...
        'Position',[5 5 style.FigureWidthExtraWideCm style.FigureHeightCm]);
 
-tiledlayout(1,2)
-
-% --- Angle of Attack ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(1,2,1); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.alpha, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.alpha','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.alpha','Color',[0.6 0.6 1])
 end
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Angle of Attack $\alpha$ (deg)','Interpreter',style.Interpreter)
-title('Angle of Attack Response','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Angle of Attack $\alpha$ (deg)')
+title('Angle of Attack Response')
 
-% --- Sideslip ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(1,2,2); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.beta, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.beta','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.beta','Color',[0.6 0.6 1])
 end
 
-yline(0,'k--','LineWidth',1)
+plot([t(1) t(end)],[0 0],'k--')
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Sideslip $\beta$ (deg)','Interpreter',style.Interpreter)
-title('Sideslip Response','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Sideslip $\beta$ (deg)')
+title('Sideslip Response')
 
 if SAVE_EPS
-    exportgraphics(gcf, ...
-        fullfile(OUTDIR, ['AoA_Sideslip_Response' suffix '.pdf']), ...
-        'ContentType','vector')
+    savePDF(gcf, fullfile(OUTDIR,['AoA_Sideslip_Response' suffix]))
 end
-
 
 %% ========================================================================
 % LOAD FACTOR + PITCH RATE
@@ -150,45 +127,35 @@ figure('Color',style.Background,...
        'Units','centimeters',...
        'Position',[5 5 style.FigureWidthExtraWideCm style.FigureHeightCm]);
 
-tiledlayout(1,2)
-
-% --- Load Factor nz ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(1,2,1); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.nz, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.nz','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.nz','Color',[0.6 0.6 1])
 end
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Load Factor $n_z$ (-)','Interpreter',style.Interpreter)
-title('Load Factor Response','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Load Factor $n_z$')
+title('Load Factor Response')
 
+subplot(1,2,2); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
-% --- Pitch Rate q ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
-
-% Nominal q (2nd rate)
 q_nom = squeeze(OUT.nom.rates(2,:));
-
 plot(t, q_nom, 'r', 'LineWidth',style.LineWidth)
 
 if munc && ~isempty(OUT.unc)
-    q_unc = squeeze(OUT.unc.rates(:,2,:));   % [N x time]
-    plot(t, q_unc','Color',[0 0 1 0.15])
+    q_unc = squeeze(OUT.unc.rates(:,2,:));
+    plot(t, q_unc','Color',[0.6 0.6 1])
 end
 
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Pitch Rate $q$ (deg/s)','Interpreter',style.Interpreter)
-title('Pitch Rate Response','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Pitch Rate $q$ (deg/s)')
+title('Pitch Rate Response')
 
 if SAVE_EPS
-    exportgraphics(gcf, ...
-        fullfile(OUTDIR, ['LoadFactor_PitchRate' suffix '.pdf']), ...
-        'ContentType','vector')
+    savePDF(gcf, fullfile(OUTDIR,['LoadFactor_PitchRate' suffix]))
 end
 
 %% ========================================================================
@@ -199,49 +166,47 @@ figure('Color',style.Background,...
        'Units','centimeters',...
        'Position',[5 5 style.FigureWidthCm style.FigureHeightWideCm]);
 
-tiledlayout(3,1)
-
-% --- Elevon Left ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(3,1,1); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.aileron, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.aileron','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.aileron','Color',[0.6 0.6 1])
 end
+ylabel('Aileron (deg)')
 
-ylabel('Aileron (deg)','Interpreter',style.Interpreter)
-
-% --- Elevon Right ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(3,1,2); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.elevator, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.elevator','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.elevator','Color',[0.6 0.6 1])
 end
+ylabel('Elevator (deg)')
 
-ylabel('Elevator (deg)','Interpreter',style.Interpreter)
-
-% --- Rudder ---
-nexttile; hold on; grid on
-set(gca,'FontSize',style.FontSize,'FontWeight',style.FontWeight)
+subplot(3,1,3); hold on; grid on
+set(gca,'FontSize',style.FontSize)
 
 plot(t, OUT.nom.rudder, 'r', 'LineWidth',style.LineWidth)
-
 if munc && ~isempty(OUT.unc)
-    plot(t, OUT.unc.rudder','Color',[0 0 1 0.15])
+    plot(t, OUT.unc.rudder','Color',[0.6 0.6 1])
 end
-
-xlabel('Time (s)','Interpreter',style.Interpreter)
-ylabel('Rudder (deg)','Interpreter',style.Interpreter)
+xlabel('Time (s)')
+ylabel('Rudder (deg)')
 
 if SAVE_EPS
-    exportgraphics(gcf, ...
-        fullfile(OUTDIR, ['ControlSurfaces' suffix '.pdf']), ...
-        'ContentType','vector')
+    savePDF(gcf, fullfile(OUTDIR,['ControlSurfaces' suffix]))
 end
+
+end
+function savePDF(figHandle, filename)
+
+pos = get(figHandle,'Position');
+
+set(figHandle,'PaperUnits','centimeters')
+set(figHandle,'PaperPositionMode','auto')
+set(figHandle,'PaperSize',[pos(3) pos(4)])
+
+print(figHandle, filename, '-dpdf')
 
 end
