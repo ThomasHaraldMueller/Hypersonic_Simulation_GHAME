f31909191c9d83efeb06fff38d65e9f86f94885a
Thomas Mueller <thomas-harald.mueller1@outlook.de>
2026-02-19 10:50:08 +0100

    Updated it for uncertainty

--- Helper Functions/Miscellaneous/Print_Mission_Description.m
+++ Helper Functions/Miscellaneous/Print_Mission_Description.m
@@ -7,39 +7,45 @@ function Print_Mission_Description(Sim, IC)
 
     % Start description with flight conditions
     desc = sprintf("Simulation mission setup:\n");
-    desc = desc + sprintf("  - Airspeed: %.1f m/s\n", IC.dvba);
-    desc = desc + sprintf("  - Altitude: %.1f m\n", IC.alt);
+    desc =     desc + sprintf("  - Airspeed:           %.1f m/s\n", IC.dvba);
+    desc =     desc + sprintf("  - Altitude:           %.1f m\n", IC.alt);
+    desc =     desc + sprintf("  - Fuel fraction:      %.1f \n\n", Sim.vehicle.fuel.fraction); 
 
+    desc = desc + sprintf("Simulation modes:\n");
     % Wind model
-    switch Sim.mode.mwind
-        case 1
-            desc = desc + sprintf("  - Wind: Enabled (constant wind model)\n");
-        case 2
-            desc = desc + sprintf("  - Wind: Enabled (linear shear model)\n");
-        otherwise
-            desc = desc + sprintf("  - Wind: Disabled\n");
+    if Sim.mode.mwind == 1
+        desc = desc + sprintf("  - Wind:               Enabled (constant wind model)\n");
+    else
+        desc = desc + sprintf("  - Wind:               Disabled\n");
     end
 
     % Turbulence
     if Sim.mode.mturb == 1
-        desc = desc + sprintf("  - Turbulence: Enabled\n");
+        desc = desc + sprintf("  - Turbulence:         Enabled\n");
     else
-        desc = desc + sprintf("  - Turbulence: Disabled\n");
+        desc = desc + sprintf("  - Turbulence:         Disabled\n");
     end
 
     % Sensor noise
     if Sim.mode.mnoise == 1
-        desc = desc + sprintf("  - Sensor noise: Enabled\n");
+        desc = desc + sprintf("  - Sensor noise:       Enabled\n");
     else
-        desc = desc + sprintf("  - Sensor noise: Disabled\n");
-    end
+        desc = desc + sprintf("  - Sensor noise:       Disabled\n");
+    end 
 
     % Control perturbations
     if Sim.mode.mperturbations == 1
-        desc = desc + sprintf("  - Control perturbations: Enabled\n");
+        desc = desc + sprintf("  - Act. perturbations: Enabled\n");
     else
-        desc = desc + sprintf("  - Control perturbations: Disabled\n");
+        desc = desc + sprintf("  - Act. perturbations: Disabled\n");
     end
+
+    if Sim.mode.muncertainty == 1
+        desc = desc + sprintf("  - Uncertainty:        Enabled\n");
+    else
+        desc = desc + sprintf("  - Uncertainty:        Disabled\n");
+    end
+
 
     % Print block
     fprintf("\n============================================================\n");
